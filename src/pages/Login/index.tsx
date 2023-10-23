4f5cc4224e85e04a833de7231f896df44d8f9666
ispadilha <israel.padilha@gmail.com>
2023-10-23 10:34:52 -0300

    style: login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { Box, Button, CircularProgress, TextField } from "@mui/material"
+import { Box, Button, Checkbox, CircularProgress, TextField } from "@mui/material"
 import { useNavigate } from "react-router-dom"
 import { Form, Formik } from "formik"
 import { useIo } from "../../hooks/useIo"
@@ -29,53 +29,122 @@ export const Login: React.FC<LoginProps> = ({}) => {
         setLoading(true)
     }
 
+    io.on("user:login", (user) => {
+        setLoading(false)
+        console.log(user)
+        navigate('/panel')
+    })
+
     return (
         <Box
             sx={{
-                width: "100vw"
+                // width: "100vw"
             }}
             >
             <Box>
-                <img src="https://chbsistemas.contentools.com/api/v1/media/174577/download/" alt="" />
+                <img src="/src/assets/whitelabel-background-startja-login.webp" alt=""
+                    style={{
+                        height: "100vh",
+                        width: "60vw",
+                        objectFit: "cover",
+                        objectPosition: "0"
+                    }}
+                />
             </Box>
-            <Formik initialValues={initialValues} onSubmit={(values) => handleLogin(values)}>
-                {({ values, handleChange }) => (
-                    <Form>
-                        <Box
-                            sx={{
-                                flexDirection: "column",
-                                margin: "auto"
-                            }}
-                        >
-                            <TextField
-                                name="email"
-                                label="E-mail"
-                                value={values.email}
-                                onChange={handleChange}
+            <Box
+                sx={{
+                    flexDirection: "column",
+                    backgroundColor: "white",
+                    width: "40vw",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    gap: "2vw"
+                }}
+            >
+                <img src="/src/assets/whitelabel-logo-startja.webp" alt=""
+                    style={{
+                        width: "30vw",
+                        objectFit: "contain"
+                    }}
+                />
+
+                <p
+                    style={{
+                        color: "black",
+                        fontSize: "1.3rem"
+                    }}
+                >
+                    Levando soluções ao empreendedor do Agro
+                </p>
+                <Formik initialValues={initialValues} onSubmit={(values) => handleLogin(values)}>
+                    {({ values, handleChange }) => (
+                        <Form>
+                            <Box
                                 sx={{
-                                    backgroundColor: "white"
+                                    flexDirection: "column",
+                                    gap: "2vw",
+                                    width: "30vw"
                                 }}
-                            />
-                            <TextField
-                                type="password"
-                                name="password"
-                                label="Senha"
-                                value={values.password}
-                                onChange={handleChange}
-                                sx={{
-                                    backgroundColor: "white"
-                                }}
-                            />
-                            <Button
-                                variant="contained"
-                                type="submit"
                             >
-                                {loading ? <CircularProgress size={30} sx={{ color: "#fff", fontSize: "2vw" }} /> : "Entrar"}
-                            </Button>
-                        </Box>
-                    </Form>
-                )}
-            </Formik>
+                                <TextField
+                                    name="email"
+                                    label="E-mail"
+                                    value={values.email}
+                                    onChange={handleChange}
+                                    sx={{
+                                        backgroundColor: "white"
+                                    }}
+                                />
+                                <TextField
+                                    type="password"
+                                    name="password"
+                                    label="Senha"
+                                    value={values.password}
+                                    onChange={handleChange}
+                                    sx={{
+                                        backgroundColor: "white"
+                                    }}
+                                />
+                                <Box
+                                    sx={{
+                                        justifyContent: "space-between",
+                                        alignItems: "center"
+                                    }}
+                                >
+                                    <Box
+                                        sx={{
+                                            gap: "1vw"
+                                        }}
+                                    >
+                                        <Checkbox
+                                            sx={{
+                                                padding: 0
+                                            }}
+                                        />
+                                        <p
+                                            style={{
+                                                color: "black"
+                                            }}
+                                        >
+                                            Manter conectado
+                                        </p>
+                                    </Box>
+                                    <Button
+                                        variant="contained"
+                                        type="submit"
+                                        sx={{
+                                            borderRadius: "4vw",
+                                            textTransform: "capitalize"
+                                        }}
+                                    >
+                                        {loading ? <CircularProgress size={30} sx={{ color: "#fff", fontSize: "2vw" }} /> : "Entrar"}
+                                    </Button>
+                                </Box>
+                            </Box>
+                        </Form>
+                    )}
+                </Formik>
+            </Box>
         </Box>
     )
 }
