b8a5543aabb20a4098c66f062d8f858a4c967995
ispadilha <israel.padilha@gmail.com>
2023-10-23 12:20:29 -0300

    feat: user provider

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,5 +1,6 @@
 import { Snackbar, SnackbarProvider } from "burgos-snackbar"
 import { IoProvider } from "./contexts/ioContext"
+import { UserProvider } from "./contexts/userContext"
 
 interface ProvidersProps {
     children: React.ReactNode
@@ -9,8 +10,10 @@ export const Providers: React.FC<ProvidersProps> = ({ children }) => {
     return (
         <SnackbarProvider>
             <IoProvider>
-                <Snackbar />
-                {children}
+                <UserProvider>
+                    <Snackbar />
+                    {children}
+                </UserProvider>
             </IoProvider>
         </SnackbarProvider>
     )
