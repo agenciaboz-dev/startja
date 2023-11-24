3acecc7ab11bed382690df998e0978dfd416468e
nandobfer <nandobfer@yahoo.com.br>
2023-11-24 16:48:36 -0300

    update: header menu items

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,78 +5,40 @@ import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined
 import ArrowDropDownOutlinedIcon from "@mui/icons-material/ArrowDropDownOutlined"
 import { useHeader } from "../../hooks/useHeader"
 import { useMenus } from "../../hooks/useMenus"
+import { useUser } from "../../hooks/useUser"
 
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = ({}) => {
     const { title } = useHeader()
     const menus = useMenus()
+    const { user } = useUser()
 
     const [menuAnchorEl, setMenuAnchorEl] = React.useState<null | HTMLElement>(null)
     const menu_opened = Boolean(menuAnchorEl)
 
     return (
-        <Box
-            sx={{
-                justifyContent: "space-between",
-                alignItems: "center",
-                width: "100%"
-            }}>
+        <Box sx={{ justifyContent: "space-between", alignItems: "center", width: "100%" }}>
             <h1>{title}</h1>
-            <Box
-                sx={{
-                    gap: "2rem",
-                    alignItems: "center"
-                }}>
-                <Box
+            <Box sx={{ alignItems: "center", height: "100%" }}>
+                <MenuItem
                     sx={{
                         alignItems: "center",
-                        gap: "0.5rem"
+                        gap: "0.5rem",
+                        paddingRight: "2vw",
+                        height: "100%",
+                        borderRight: "1px solid"
                     }}>
-                    <HelpOutlineOutlinedIcon
-                        sx={{
-                            fill: "#323232",
-                            height: "2rem",
-                            width: "2rem"
-                        }}
-                    />
-                    <p
-                        style={{
-                            fontWeight: "lighter"
-                        }}>
-                        Ajuda
-                    </p>
-                </Box>
-                <hr
-                    style={{
-                        height: "2rem"
-                    }}
-                />
-                <MenuItem sx={{ alignItems: "center", gap: "0.5rem", width: "15vw" }} onClick={(event) => setMenuAnchorEl(event.currentTarget)}>
-                    <AccountCircleOutlinedIcon
-                        sx={{
-                            fill: "#323232",
-                            height: "2rem",
-                            width: "2rem"
-                        }}
-                    />
-                    <Box
-                        sx={{
-                            flexDirection: "column"
-                        }}>
-                        <p
-                            style={{
-                                fontWeight: "lighter"
-                            }}>
-                            [Nome do Usuário]
-                        </p>
-                        <p
-                            style={{
-                                fontSize: "0.8rem",
-                                fontWeight: "bold"
-                            }}>
-                            Menus e configurações
-                        </p>
+                    <HelpOutlineOutlinedIcon sx={{ fill: "#323232", height: "2rem", width: "2rem" }} />
+                    <p style={{ fontWeight: "lighter" }}>Ajuda</p>
+                </MenuItem>
+                <MenuItem
+                    sx={{ alignItems: "center", gap: "0.5rem", width: "15vw", paddingLeft: "2vw" }}
+                    onClick={(event) => setMenuAnchorEl(event.currentTarget)}>
+                    <AccountCircleOutlinedIcon sx={{ fill: "#323232", height: "2rem", width: "2rem" }} />
+                    <Box sx={{ flexDirection: "column" }}>
+                        <p style={{ fontWeight: "lighter" }}>{user?.name}</p>
+                        <p style={{ fontSize: "0.8rem", fontWeight: "bold" }}>Menus e configurações</p>
                     </Box>
                     <ArrowDropDownOutlinedIcon />
                 </MenuItem>
