747a659ce76c5d356724272f497aeffd663225ac
ispadilha <israel.padilha@gmail.com>
2023-12-14 08:49:27 -0300

    style: registry natures page for mobile

--- src/pages/Panel/subpages/Registry/subpages/Natures/index.tsx
+++ src/pages/Panel/subpages/Registry/subpages/Natures/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { Box, Button } from "@mui/material"
+import { Box, Button, useMediaQuery } from "@mui/material"
 import AddOutlinedIcon from "@mui/icons-material/AddOutlined"
 import { NaturesListHeader } from "../../../../../../components/NaturesList/NaturesListHeader"
 import { NaturesList } from "../../../../../../components/NaturesList"
@@ -13,6 +13,7 @@ import normalize from "../../../../../../tools/normalize"
 interface NaturesProps {}
 
 export const Natures: React.FC<NaturesProps> = ({}) => {
+    const isMobile = useMediaQuery("(orientation: portrait)")
     const natures = useNature()
     const header = useHeader()
     const io = useIo()
@@ -47,8 +48,10 @@ export const Natures: React.FC<NaturesProps> = ({}) => {
             />
             <Box
                 sx={{
-                    height: "100%",
-                    width: "100%",
+                    flex: 1,
+                    overflow: isMobile ? "scroll" : "",
+                    padding: isMobile ? "1vw 5vw" : "",
+                    margin: isMobile ? "0 -5vw" : "",
                 }}
             >
                 {emptyNaturesList && (
@@ -86,12 +89,12 @@ export const Natures: React.FC<NaturesProps> = ({}) => {
                     <Box
                         sx={{
                             flex: 1,
-                            padding: "1vw 1.5vw 1vw 0.5vw",
                             boxShadow: "0 2px 2px 2px #d1d1d1",
                             backgroundColor: "white",
                             borderRadius: "20px",
                             flexDirection: "column",
-                            width: "100%",
+                            padding: isMobile ? "5vw" : "1vw 1.5vw 1vw 0.5vw",
+                            width: isMobile ? "fit-content" : "100%",
                         }}
                     >
                         <NaturesListHeader />
