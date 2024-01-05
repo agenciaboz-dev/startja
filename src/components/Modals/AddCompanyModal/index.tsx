29ce052873be48ba6ecb78fb4f45434a229df032
ispadilha <israel.padilha@gmail.com>
2024-01-05 15:21:29 -0300

    style: updated addCompanyModal

--- src/components/Modals/AddCompanyModal/index.tsx
+++ src/components/Modals/AddCompanyModal/index.tsx
@@ -1,5 +1,17 @@
 import React, { useState, useEffect } from "react"
-import { Dialog, DialogTitle, DialogContent, DialogActions, Button, TextField, Grid, CircularProgress, useMediaQuery, MenuItem } from "@mui/material"
+import {
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+    Button,
+    TextField,
+    Grid,
+    CircularProgress,
+    useMediaQuery,
+    MenuItem,
+    Box,
+} from "@mui/material"
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined"
 import { useIo } from "../../../hooks/useIo"
 import { useFormik } from "formik"
@@ -30,16 +42,16 @@ const AddCompanyModal: React.FC<AddCompanyModalProps> = ({ open, onClose }) => {
             adjunct: "",
             number: "",
             cep: "",
-            email: "r",
+            email: "",
             phone: "",
             businessName: "",
-            customerId: user?.id || 0
+            customerId: user?.id || 0,
         },
         onSubmit: (values) => {
             console.log(values)
             setLoading(true)
             io.emit("company:create", values)
-        }
+        },
     })
 
     const [loading, setLoading] = useState(false)
@@ -67,122 +79,221 @@ const AddCompanyModal: React.FC<AddCompanyModalProps> = ({ open, onClose }) => {
             open={open}
             onClose={onClose}
             sx={{
-                justifyContent: "center"
+                justifyContent: "center",
             }}
             PaperProps={{
                 sx: {
                     borderRadius: "20px",
-                    minWidth: "90vw"
-                }
-            }}>
+                    minWidth: "90vw",
+                },
+            }}
+        >
             <form style={{ display: "contents" }} onSubmit={formik.handleSubmit}>
-                <DialogTitle>Adicionar Empresa</DialogTitle>
+                <DialogTitle>Adicionar Pessoa ou Empresa</DialogTitle>
                 <CloseOutlinedIcon
                     sx={{
                         position: "absolute",
                         top: isMobile ? "5vw" : "1vw",
                         right: isMobile ? "5vw" : "1vw",
-                        cursor: "pointer"
+                        cursor: "pointer",
                     }}
                     onClick={onClose}
                 />
-                <DialogContent
-                    sx={{
-                        paddingTop: 0
-                    }}>
-                    <Grid container spacing={2}>
-                        <Grid item xs={isMobile ? 12 : 3}>
-                            <TextField required label="Tipo" fullWidth value={formik.values.type} name="type" onChange={formik.handleChange} />
-                        </Grid>
-                        <Grid item xs={isMobile ? 12 : 3}>
-                            <TextField required label="Nome" fullWidth value={formik.values.name} name="name" onChange={formik.handleChange} />
-                        </Grid>
-                        <Grid item xs={isMobile ? 12 : 3}>
-                            <TextField
-                                required
-                                label="Documento"
-                                fullWidth
-                                value={formik.values.document}
-                                name="document"
-                                onChange={formik.handleChange}
-                            />
-                        </Grid>
-                        <Grid item xs={isMobile ? 12 : 3}>
-                            <TextField
-                                required
-                                label="inscricaoEstadual"
-                                fullWidth
-                                value={formik.values.inscricaoEstadual}
-                                name="inscricaoEstadual"
-                                onChange={formik.handleChange}
-                            />
-                        </Grid>
-                        <Grid item xs={isMobile ? 12 : 3}>
-                            <TextField
-                                fullWidth
-                                label="Indicador de inscrição estadual"
-                                value={formik.values.indicadorEstadual}
-                                name=".indicadorEstadual"
-                                onChange={formik.handleChange}
-                                select>
-                                <MenuItem value={1}>Contribuinte ICMS</MenuItem>
-                                <MenuItem value={2}>Contribuinte isento de Inscrição no cadastro de Contribuintes do ICMS</MenuItem>
-                                <MenuItem value={9}>
-                                    Não Contribuinte, que pode ou não possuir Inscrição Estadual no Cadastro de Contribuintes do ICMS
-                                </MenuItem>
-                            </TextField>
-                        </Grid>
-                        <Grid item xs={isMobile ? 12 : 3}>
-                            <TextField required label="Cidade" fullWidth value={formik.values.city} name="city" onChange={formik.handleChange} />
-                        </Grid>
-                        <Grid item xs={isMobile ? 12 : 3}>
-                            <TextField required label="Estado" fullWidth value={formik.values.state} name="state" onChange={formik.handleChange} />
-                        </Grid>
-                        <Grid item xs={isMobile ? 12 : 3}>
-                            <TextField
-                                required
-                                label="Bairro"
-                                fullWidth
-                                value={formik.values.district}
-                                name="district"
-                                onChange={formik.handleChange}
-                            />
-                        </Grid>
-                        <Grid item xs={isMobile ? 12 : 3}>
-                            <TextField required label="Rua" fullWidth value={formik.values.street} name="street" onChange={formik.handleChange} />
-                        </Grid>
-                        <Grid item xs={isMobile ? 12 : 3}>
-                            <TextField label="Complemento" fullWidth value={formik.values.adjunct} name="adjunct" onChange={formik.handleChange} />
-                        </Grid>
-                        <Grid item xs={isMobile ? 12 : 3}>
-                            <TextField required label="Número" fullWidth value={formik.values.number} name="number" onChange={formik.handleChange} />
-                        </Grid>
-                        <Grid item xs={isMobile ? 12 : 3}>
-                            <TextField required label="CEP" fullWidth value={formik.values.cep} name="cep" onChange={formik.handleChange} />
-                        </Grid>
-                        <Grid item xs={isMobile ? 12 : 3}>
-                            <TextField required label="E-mail" fullWidth value={formik.values.email} name="email" onChange={formik.handleChange} />
-                        </Grid>
-                        <Grid item xs={isMobile ? 12 : 3}>
-                            <TextField required label="Telefone" fullWidth value={formik.values.phone} name="phone" onChange={formik.handleChange} />
-                        </Grid>
-                        <Grid item xs={isMobile ? 12 : 3}>
-                            <TextField
-                                required
-                                label="Nome Fantasia"
-                                fullWidth
-                                value={formik.values.businessName}
-                                name="businessName"
-                                onChange={formik.handleChange}
-                            />
-                        </Grid>
-                    </Grid>
+                <DialogContent>
+                    <Box
+                        sx={{
+                            flexDirection: "column",
+                            gap: isMobile ? "10vw" : "2vw",
+                        }}
+                    >
+                        <Box
+                            sx={{
+                                flexDirection: "column",
+                                gap: isMobile ? "5vw" : "1vw",
+                            }}
+                        >
+                            <h3>Dados de Identificação</h3>
+                            <Grid container spacing={2}>
+                                <Grid item xs={isMobile ? 12 : 6}>
+                                    <TextField
+                                        required
+                                        label="Nome"
+                                        fullWidth
+                                        value={formik.values.name}
+                                        name="name"
+                                        onChange={formik.handleChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={isMobile ? 12 : 6}>
+                                    <TextField
+                                        required
+                                        label="Nome Fantasia"
+                                        fullWidth
+                                        value={formik.values.businessName}
+                                        name="businessName"
+                                        onChange={formik.handleChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={isMobile ? 12 : 3}>
+                                    <TextField
+                                        required
+                                        label="Tipo"
+                                        fullWidth
+                                        value={formik.values.type}
+                                        name="type"
+                                        onChange={formik.handleChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={isMobile ? 12 : 3}>
+                                    <TextField
+                                        required
+                                        label="Documento"
+                                        fullWidth
+                                        value={formik.values.document}
+                                        name="document"
+                                        onChange={formik.handleChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={isMobile ? 12 : 3}>
+                                    <TextField
+                                        required
+                                        label="inscricaoEstadual"
+                                        fullWidth
+                                        value={formik.values.inscricaoEstadual}
+                                        name="inscricaoEstadual"
+                                        onChange={formik.handleChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={isMobile ? 12 : 3}>
+                                    <TextField
+                                        fullWidth
+                                        label="Indicador de inscrição estadual"
+                                        value={formik.values.indicadorEstadual}
+                                        name=".indicadorEstadual"
+                                        onChange={formik.handleChange}
+                                        select
+                                    >
+                                        <MenuItem value={1}>Contribuinte ICMS</MenuItem>
+                                        <MenuItem value={2}>Contribuinte isento de Inscrição no cadastro de Contribuintes do ICMS</MenuItem>
+                                        <MenuItem value={9}>
+                                            Não Contribuinte, que pode ou não possuir Inscrição Estadual no Cadastro de Contribuintes do ICMS
+                                        </MenuItem>
+                                    </TextField>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                        <hr />
+                        <Box
+                            sx={{
+                                flexDirection: "column",
+                                gap: isMobile ? "5vw" : "1vw",
+                            }}
+                        >
+                            <h3>Contato</h3>
+                            <Grid container spacing={2}>
+                                <Grid item xs={isMobile ? 12 : 6}>
+                                    <TextField
+                                        required
+                                        label="E-mail"
+                                        fullWidth
+                                        value={formik.values.email}
+                                        name="email"
+                                        onChange={formik.handleChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={isMobile ? 12 : 6}>
+                                    <TextField
+                                        required
+                                        label="Telefone"
+                                        fullWidth
+                                        value={formik.values.phone}
+                                        name="phone"
+                                        onChange={formik.handleChange}
+                                    />
+                                </Grid>
+                            </Grid>
+                        </Box>
+                        <hr />
+                        <Box
+                            sx={{
+                                flexDirection: "column",
+                                gap: isMobile ? "5vw" : "1vw",
+                            }}
+                        >
+                            <h3>Endereço</h3>
+                            <Grid container spacing={2}>
+                                <Grid item xs={isMobile ? 12 : 6}>
+                                    <TextField
+                                        required
+                                        label="Rua"
+                                        fullWidth
+                                        value={formik.values.street}
+                                        name="street"
+                                        onChange={formik.handleChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={isMobile ? 12 : 3}>
+                                    <TextField
+                                        required
+                                        label="Número"
+                                        fullWidth
+                                        value={formik.values.number}
+                                        name="number"
+                                        onChange={formik.handleChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={isMobile ? 12 : 3}>
+                                    <TextField
+                                        label="Complemento"
+                                        fullWidth
+                                        value={formik.values.adjunct}
+                                        name="adjunct"
+                                        onChange={formik.handleChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={isMobile ? 12 : 3}>
+                                    <TextField required label="CEP" fullWidth value={formik.values.cep} name="cep" onChange={formik.handleChange} />
+                                </Grid>
+                                <Grid item xs={isMobile ? 12 : 3}>
+                                    <TextField
+                                        required
+                                        label="Bairro"
+                                        fullWidth
+                                        value={formik.values.district}
+                                        name="district"
+                                        onChange={formik.handleChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={isMobile ? 12 : 3}>
+                                    <TextField
+                                        required
+                                        label="Cidade"
+                                        fullWidth
+                                        value={formik.values.city}
+                                        name="city"
+                                        onChange={formik.handleChange}
+                                    />
+                                </Grid>
+                                <Grid item xs={isMobile ? 12 : 3}>
+                                    <TextField
+                                        required
+                                        label="Estado"
+                                        fullWidth
+                                        value={formik.values.state}
+                                        name="state"
+                                        onChange={formik.handleChange}
+                                    />
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
                 </DialogContent>
                 <DialogActions
                     sx={{
                         margin: isMobile ? "0" : "0.5vw",
-                        padding: isMobile ? "5vw" : ""
-                    }}>
+                        padding: isMobile ? "5vw" : "",
+                    }}
+                >
                     <Button
                         onClick={onClose}
                         color="secondary"
@@ -190,8 +301,9 @@ const AddCompanyModal: React.FC<AddCompanyModalProps> = ({ open, onClose }) => {
                         sx={{
                             borderRadius: "20px",
                             color: "white",
-                            textTransform: "unset"
-                        }}>
+                            textTransform: "unset",
+                        }}
+                    >
                         Cancelar
                     </Button>
                     <Button
@@ -201,8 +313,9 @@ const AddCompanyModal: React.FC<AddCompanyModalProps> = ({ open, onClose }) => {
                         sx={{
                             borderRadius: "20px",
                             color: "white",
-                            textTransform: "unset"
-                        }}>
+                            textTransform: "unset",
+                        }}
+                    >
                         {loading ? <CircularProgress size="1.5rem" color="inherit" /> : "Adicionar"}
                     </Button>
                 </DialogActions>
