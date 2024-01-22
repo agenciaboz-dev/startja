c89a14de313e001754d1e62322619789d6a24070
nandobfer <nandobfer@yahoo.com.br>
2024-01-22 10:59:51 -0300

    update: started refac of operation options

--- src/components/Modals/AddNatureModal/index.tsx
+++ src/components/Modals/AddNatureModal/index.tsx
@@ -27,25 +27,38 @@ const AddNatureModal: React.FC<AddNatureModalProps> = ({ open, onClose }) => {
         const selectedOperation = event.target.value
         setSelectedOperation(selectedOperation)
 
-        if (
-            selectedOperation === "Compra de mercadorias" ||
-            selectedOperation === "Retorno de mercadoria" ||
-            selectedOperation === "Transferência de entrada"
-        ) {
-            setSelectedType("Entrada")
-            setSelectedFinality("Normal")
-        } else if (selectedOperation === "Devolução de venda") {
-            setSelectedType("Entrada")
-            setSelectedFinality("Devolução")
-        } else if (selectedOperation === "Remessa de mercadoria" || selectedOperation === "Transferência de saída" || selectedOperation === "Venda") {
-            setSelectedType("Saída")
-            setSelectedFinality("Normal")
-        } else if (selectedOperation === "Devolução de compra") {
-            setSelectedType("Saída")
-            setSelectedFinality("Devolução")
-        } else if (selectedOperation === "Anulação de valores") {
-            setSelectedType("Saída")
-            setSelectedFinality("Ajuste")
+        const options = [
+            { operation: "Compra de mercadorias", type: "Entrada", finality: "Normal" },
+            { operation: "Retorno de mercadoria", type: "Entrada", finality: "Normal" },
+            { operation: "Anulação de valores", type: "Saída", finality: "Ajuste" }
+        ]
+
+        //     if (
+        //         selectedOperation === "Compra de mercadorias" ||
+        //         selectedOperation === "Retorno de mercadoria" ||
+        //         selectedOperation === "Transferência de entrada"
+        //     ) {
+        //         setSelectedType("Entrada")
+        //         setSelectedFinality("Normal")
+        //     } else if (selectedOperation === "Devolução de venda") {
+        //         setSelectedType("Entrada")
+        //         setSelectedFinality("Devolução")
+        //     } else if (selectedOperation === "Remessa de mercadoria" || selectedOperation === "Transferência de saída" || selectedOperation === "Venda") {
+        //         setSelectedType("Saída")
+        //         setSelectedFinality("Normal")
+        //     } else if (selectedOperation === "Devolução de compra") {
+        //         setSelectedType("Saída")
+        //         setSelectedFinality("Devolução")
+        //     } else if (selectedOperation === "Anulação de valores") {
+        //         setSelectedType("Saída")
+        //         setSelectedFinality("Ajuste")
+        //     }
+        // }
+
+        const operation = options.find((item) => item.operation == selectedOperation)
+        if (operation) {
+            setSelectedType(operation.type)
+            setSelectedFinality(operation.finality)
         }
     }
 
