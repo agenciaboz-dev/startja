6e4363aa070be3ded80c8f4c6f6f2769da09b8d5
nandobfer <nandobfer@yahoo.com.br>
2024-01-25 11:25:18 -0300

    update: sorting natures

--- src/components/Lists/NaturesList/index.tsx
+++ src/components/Lists/NaturesList/index.tsx
@@ -14,12 +14,13 @@ export const NaturesList: React.FC<NaturesListProps> = ({ natures }) => {
                 flexDirection: "column",
                 alignItems: "center",
                 margin: isMobile ? "2vw 0" : "0.5vw 0",
-                gap: isMobile ? "2vw" : "",
-            }}
-        >
-            {natures.map((nature) => (
-                <NatureRow key={nature.id} nature={nature} />
-            ))}
+                gap: isMobile ? "2vw" : ""
+            }}>
+            {natures
+                .sort((a, b) => a.id - b.id)
+                .map((nature) => (
+                    <NatureRow key={nature.id} nature={nature} />
+                ))}
         </Box>
     )
 }
