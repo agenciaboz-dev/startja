10e7d3c4ca12c0aaef59b791c23e4b0165cbee21
nandobfer <nandobfer@yahoo.com.br>
2024-01-29 12:43:09 -0300

    fix: initial cfop

--- src/components/Modals/AddInvoiceModal/ProductForm.tsx
+++ src/components/Modals/AddInvoiceModal/ProductForm.tsx
@@ -9,6 +9,7 @@ import { useCurrencyMask, useNumberMask } from "burgos-masks"
 import MaskedInput from "../../MaskedInput"
 import { unmaskCurrency, unmaskNumber } from "../../../tools/unmaskNumber"
 import { TaxValues } from "../../TaxValues"
+import { useSnackbar } from "burgos-snackbar"
 
 interface ProductFormProps {
     addProduct: (product: InvoiceProduct) => void
@@ -24,14 +25,14 @@ export const ProductForm: React.FC<ProductFormProps> = ({ addProduct, focusNFEIn
     const isMobile = useMediaQuery("(orientation: portrait)")
     const number_mask = useNumberMask({})
     const { list } = useProduct()
+    const { snackbar } = useSnackbar()
 
     const [productFormDisplay, setProductFormDisplay] = useState("produto")
     const [currentProduct, setCurrentProduct] = useState<Product>(list[0])
 
     const formik = useFormik<InvoiceProduct>({
         initialValues: {
-            aliquota: 0,
-            cfop: 0,
+            cfop: 1101,
             cofins_situacao_tributaria: "01",
             icms_modalidade_base_calculo: 0,
             icms_origem: list[0].icmsOrigin,
@@ -51,6 +52,14 @@ export const ProductForm: React.FC<ProductFormProps> = ({ addProduct, focusNFEIn
             informacoes_adicionais_item: ""
         },
         onSubmit: (values) => {
+            if (!values.quantidade) {
+                snackbar({ severity: "error", text: "insira a quantidade" })
+                return
+            }
+            if (!values.quantidade) {
+                snackbar({ severity: "error", text: "insira o valor unitário" })
+                return
+            }
             const data = {
                 ...values,
                 unidade_tributavel: values.unidade_comercial,
